34c4df3b1c112913eefc60fc6873b71156f9b346
Kristyn Le <kristynle0608@gmail.com>
2022-05-05 16:46:13 -0300

    Add Ground Check

--- Squishy Squishy - A 2D Platformer Game/Assets/Scripts/PlayerMovement.cs
+++ Squishy Squishy - A 2D Platformer Game/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,11 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D rb;
-    private BoxCollider2D collider;
+    private BoxCollider2D coll;
     private Animator anim;
     private SpriteRenderer rend;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
@@ -22,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         rend = GetComponent<SpriteRenderer>();
-        collider = GetComponent<BoxCollider2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -32,7 +34,7 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2 (dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(0, jumpForce);
         }
@@ -73,6 +75,6 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-        Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, 0.1f, );
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 0.1f, jumpableGround);
     }
 }
