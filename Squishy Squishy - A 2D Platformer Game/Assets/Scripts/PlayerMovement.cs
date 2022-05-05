18395e76521ce5e09f0b3456eb0103b1d2c11ca8
Kristyn Le <kristynle0608@gmail.com>
2022-05-05 13:32:25 -0300

    Make Player move left and right

--- Squishy Squishy - A 2D Platformer Game/Assets/Scripts/PlayerMovement.cs
+++ Squishy Squishy - A 2D Platformer Game/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        // horizontal movement
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2 (dirX * 7f, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 7, 0);
+            rb.velocity = new Vector2(0, 14f);
         }
     }
 }
