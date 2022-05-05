d42e098c80c8de7fabbb39e9aefd29ce8243681a
Kristyn Le <kristynle0608@gmail.com>
2022-05-05 15:33:07 -0300

    Complete the state machine

--- Squishy Squishy - A 2D Platformer Game/Assets/Scripts/CameraController.cs
+++ Squishy Squishy - A 2D Platformer Game/Assets/Scripts/CameraController.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform player;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 }
