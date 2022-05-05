b71ce3b16a2ec80d03278b76f76815ec6471a04a
Kristyn Le <kristynle0608@gmail.com>
2022-05-05 15:02:17 -0300

    Added animations for running and added more code

--- Squishy Squishy - A 2D Platformer Game/Assets/Scripts/PlayerMovement.cs
+++ Squishy Squishy - A 2D Platformer Game/Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,51 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer rend;
+
+    float dirX = 0f;
+    private float moveSpeed = 7f;
+    private float jumpForce = 14f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // horizontal movement
-        float dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2 (dirX * 7f, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2 (dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(0, 14f);
+            rb.velocity = new Vector2(0, jumpForce);
+        }
+
+        AnimTransition();
+    }
+
+    private void AnimTransition()
+    {
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            rend.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            rend.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
         }
     }
 }
