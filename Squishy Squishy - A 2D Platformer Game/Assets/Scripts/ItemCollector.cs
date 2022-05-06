f4230bf914f5b356e1ea36d08fbacdea54053561
Kristyn Le <kristynle0608@gmail.com>
2022-05-06 08:58:27 -0300

    Add int cherries

--- Squishy Squishy - A 2D Platformer Game/Assets/Scripts/ItemCollector.cs
+++ Squishy Squishy - A 2D Platformer Game/Assets/Scripts/ItemCollector.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class ItemCollector : MonoBehaviour
 {
 
+    private int cherries = 0;   
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.gameObject.CompareTag("Cherry"))
         {
             Destroy(collision.gameObject);
+            cherries++;
+            Debug.Log("Cherries: " + cherries);
         }
     }
 }
