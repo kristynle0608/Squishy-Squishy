ca55e074bb84ed2436353e081572613375414385
Kristyn Le <kristynle0608@gmail.com>
2022-05-06 08:53:37 -0300

    Destroy Cherry when Collided with Player

--- Squishy Squishy - A 2D Platformer Game/Assets/Scripts/ItemCollector.cs
+++ Squishy Squishy - A 2D Platformer Game/Assets/Scripts/ItemCollector.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            Destroy(collision.gameObject);
+        }
     }
 }
