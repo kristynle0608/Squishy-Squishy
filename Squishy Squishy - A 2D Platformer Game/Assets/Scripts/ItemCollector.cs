15b24aeb010a00785c705bf8d9945834ce3f6206
Kristyn Le <kristynle0608@gmail.com>
2022-05-06 09:24:56 -0300

    Add CherriesText to ItemCollector

--- Squishy Squishy - A 2D Platformer Game/Assets/Scripts/ItemCollector.cs
+++ Squishy Squishy - A 2D Platformer Game/Assets/Scripts/ItemCollector.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
 
-    private int cherries = 0;   
+    private int cherries = 0;
+    [SerializeField] private Text cherriesText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,7 +16,8 @@ public class ItemCollector : MonoBehaviour
         {
             Destroy(collision.gameObject);
             cherries++;
-            Debug.Log("Cherries: " + cherries);
+
+            cherriesText.text = "Cherries: " + cherries;
         }
     }
 }
