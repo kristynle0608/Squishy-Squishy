553c0f5338e073c55025f3a0678170be96ff4069
Kristyn Le <kristynle0608@gmail.com>
2022-05-06 10:05:42 -0300

    Animate Player's Death

--- Squishy Squishy - A 2D Platformer Game/Assets/Scripts/PlayerLife.cs
+++ Squishy Squishy - A 2D Platformer Game/Assets/Scripts/PlayerLife.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerLife : MonoBehaviour
 {
+    private Animator anim;  
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        anim.SetTrigger("death");
     }
 }
