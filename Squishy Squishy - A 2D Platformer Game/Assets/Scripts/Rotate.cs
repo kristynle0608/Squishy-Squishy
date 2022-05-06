ca7b87c8c8be39943153d70ec8bb0c3663d7cac0
Kristyn Le <kristynle0608@gmail.com>
2022-05-06 11:42:30 -0300

    Add box collider to Saw and make it rotate

--- Squishy Squishy - A 2D Platformer Game/Assets/Scripts/Rotate.cs
+++ Squishy Squishy - A 2D Platformer Game/Assets/Scripts/Rotate.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private float speed = 2f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, 360 * speed * Time.deltaTime);
     }
 }
