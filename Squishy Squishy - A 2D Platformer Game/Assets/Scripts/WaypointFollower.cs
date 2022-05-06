a2f622cb616225e11c43995ad9749943deb0a68a
Kristyn Le <kristynle0608@gmail.com>
2022-05-06 11:03:54 -0300

    Make Moving Platform move

--- Squishy Squishy - A 2D Platformer Game/Assets/Scripts/WaypointFollower.cs
+++ Squishy Squishy - A 2D Platformer Game/Assets/Scripts/WaypointFollower.cs
@@ -6,10 +6,25 @@ public class WaypointFollower : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    [SerializeField] private float speed = 2.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // distance between current waypoint and the moving platform    
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < 0.1f)
+        {
+            currentWaypointIndex++;
+
+            // if currentWaypointIndex increments to an index that is greater than the length of the array
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime * speed);
     }
 }
